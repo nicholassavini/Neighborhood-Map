367f4a8b9db58c274d60f3aaaf50ab57d8048074
Nick Savini <nicholasmsavini@gmail.com>
2017-06-05 21:37:32 -0400

    Move locations array outside of initmap function

--- js/app.js
+++ js/app.js
@@ -1,16 +1,50 @@
 
-var map;
+// Model
+var locations = [
+        {
+            title: 'Lincoln Center',
+            location: {
+                lat: 40.7725,
+                lng: -73.9835
+            }
+        },
+        {
+            title: 'MOMA',
+            location: {
+                lat: 40.7614,
+                lng: -73.9776
+            }
+        },
+        {
+            title: 'Whitney Museum',
+            location: {
+                lat: 40.7396,
+                lng: -74.0089
+            }
+        },
+        {
+            title: 'Carnegie Hall',
+            location: {
+                lat: 40.7651,
+                lng: -73.9799
+            }
+        },
+        {
+            title: 'Met Museum',
+            location: {
+                lat: 40.7794,
+                lng: -73.963
+                2
+            }
+        }
+];
 
+var map;
 var markers = [];
 
+// View Model
+
 function initMap() {
-    var locations = [
-          {title: 'Lincoln Center', location: {lat: 40.7725, lng: -73.9835}},
-          {title: 'MOMA', location: {lat: 40.7614, lng: -73.9776}},
-          {title: 'Whitney Museum', location: {lat: 40.7396, lng: -74.0089}},
-          {title: 'Carnegie Hall', location: {lat: 40.7651, lng: -73.9799}},
-          {title: 'Met Museum', location: {lat: 40.7794, lng: -73.9632}}
-        ];
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
         center: locations[3].location
