2f0b1f26c47ffdb6ee7f092c83655b7b15e0ce7b
Nick Savini <nicholasmsavini@gmail.com>
2017-06-07 21:35:13 -0400

    Move marker generation to view model

--- js/app.js
+++ js/app.js
@@ -52,37 +52,32 @@ function initMap() {
         zoom: 14,
         center: locations[3].position
     });
-    for (var i = 0; i < locations.length; i++) {
-        var location = locations[i];
 
-        var marker = new google.maps.Marker({
-          map: map,
-          position: location.position,
-          title: location.title,
-          animation: google.maps.Animation.DROP,
-          id: i
-        });
-
-        location.marker = marker;
-    }
+    ko.applyBindings(new ViewModel());
 }
 
 var ViewModel = function() {
     var self = this;
 
     this.allLocations = ko.observableArray(locations);
+    this.filteredLocations = ko.observableArray(locations);
 
     this.setCenter = function(location) {
         map.setCenter(location.position);
         location.marker.setAnimation(google.maps.Animation.DROP);
 
     }
-
-    this.filteredLocations = ko.observableArray();
-
     this.allLocations().forEach(function(location) {
-        self.filteredLocations.push(location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: location.position,
+            title: location.title,
+            animation: google.maps.Animation.DROP
+        });
+        location.marker = marker;
     });
+
+
 
     this.filter = ko.observable('');
 
